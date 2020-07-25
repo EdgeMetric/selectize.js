7859d067cee60f04e2eb8fea99e91e13f3909e55
harismuzaffer <harismuzaffer@gmail.com>
2020-07-25 20:31:58 +0530

    build files

--- dist/js/standalone/selectize.js
+++ dist/js/standalone/selectize.js
@@ -1565,9 +1565,9 @@
 					var pastedText = self.$control_input.val();
 					if(!pastedText.match(self.settings.splitOn)){ return }
 	                
-	                if(!self.settings.keepWhitesapces) {
-	                    pastedText = $.trim(pastedText);
-	                }
+					if(!self.settings.keepWhitesapces) {
+						pastedText = $.trim(pastedText);
+					}
 	
 					var splitInput = pastedText.split(self.settings.splitOn);
 					for (var i = 0, n = splitInput.length; i < n; i++) {
@@ -2197,9 +2197,9 @@
 	
 			var self              = this;
 			var query             = $.trim(self.$control_input.val());
-	        if(!self.settings.keepWhitesapces){
-	            query = $.trim(query);
-	        }
+			if(!self.settings.keepWhitesapces){
+				query = $.trim(query);
+			}
 			var results           = self.search(query);
 			var $dropdown_content = self.$dropdown_content;
 			var active_before     = self.$activeOption && hash_key(self.$activeOption.attr('data-value'));
@@ -2722,10 +2722,10 @@
 			var self  = this;
 			var caret = self.caretPos;
 	        var value = self.$control_input.val() || '';
-	        if(!self.settings.keepWhitesapces){
-	            value = $.trim(value);
-	        }
-	        input = input || value;
+			if(!self.settings.keepWhitesapces){
+				value = $.trim(value);
+			}
+			input = input || value;
 	
 			var callback = arguments[arguments.length - 1];
 			if (typeof callback !== 'function') callback = function() {};
